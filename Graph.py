bce2fad867bd6b3f2d54334c76ef43decefcbe0e
aliyazdanifar81 <yazdanifar1381@gmail.com>
2023-09-10 16:28:30 +0330

    function "bfs" added

--- Graph.py
+++ Graph.py
@@ -17,15 +17,46 @@ class Graph:
         self.__create_graph()
 
     # User function
-    def bfs(self):
-        my_queue, res, counter = [self.__nodes[0]], [], 0
-        for key in my_queue:
-            for val in self.__graph[key]:
-                if val[0] not in my_queue:
-                    my_queue.append(val[0])
-            res.append(my_queue[counter])
-            counter += 1
-        return res
+    def bfs(self, org=None, des=None):
+        res, counter = [], 0
+        if org is None:
+            my_queue = [self.__nodes[0]]
+        else:
+            if org in self.__nodes:
+                my_queue = [org]
+            else:
+                raise Exception("Origin was NOT found")
+        if des is None:
+            for key in my_queue:
+                for val in self.__graph[key]:
+                    if val[0] not in my_queue:
+                        my_queue.append(val[0])
+                res.append(my_queue[counter])
+                counter += 1
+            return res
+        else:
+            if des not in self.__nodes:
+                raise Exception("Destination was NOT found")
+            temp_dict, flag, res = {org: [None, 0]}, 0, []
+            # temp_dict (format) = [nearest connected node, distance from origin]
+            for key in my_queue:
+                for val in self.__graph[key]:
+                    if val[0] not in my_queue:
+                        my_queue.append(val[0])
+                        temp_dict[val[0]] = [key, temp_dict[key][1] + 1]
+                    if val[0] == des:
+                        flag, res = 1, [val[0]]
+                        break
+                else:
+                    counter += 1
+                    continue
+                break
+
+            if flag:
+                while des != org:
+                    res.append(temp_dict[des][0])
+                    des = temp_dict[des][0]
+            return res[::-1]
 
     # Private functions
     def __create_graph(self):
@@ -39,8 +70,10 @@ class Graph:
                 self.__graph[data[0]].append([data[1], data[2]])
             self.__nodes = list(self.__graph.keys())
 
-    # Operator overloading\
-    def show(self):  # GUI design for his sector
+    # Operator overloading
+    def show(
+            self):  # GUI design for his sector | resource for how to use "Networkx" -->
+        # https://towardsdatascience.com/plotting-network-graphs-using-python-bc62f0d93b3f
         edge_labels = {}
         options = {
             'node_color': 'yellow',  # color of node
